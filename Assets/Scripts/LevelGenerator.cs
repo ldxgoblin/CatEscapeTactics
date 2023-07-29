b34dd1ffd45e1ede4b46fba262c1e9a668f50c2b
SHC <superhardcode666@gmail.com>
2023-07-29 16:21:20 +0200

    refactoring

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,13 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject Tile1;
-    public GameObject Tile2;
     public GameObject StartTile;
 
     [SerializeField] private GameObject[] _tiles;
@@ -21,46 +16,29 @@ public class LevelGenerator : MonoBehaviour
 
     private void Start()
     {
-        GameObject StartPlane1 = Instantiate(StartTile, transform);
-        StartPlane1.transform.position = new Vector3(7, 0, 0);
-        GameObject StartPlane2 = Instantiate(StartTile, transform);
-        StartPlane2.transform.position = new Vector3(-1, 0, 0);
-        GameObject StartPlane3 = Instantiate(StartTile, transform);
-        StartPlane3.transform.position = new Vector3(-9, 0, 0);
-        GameObject StartPlane4 = Instantiate(StartTile, transform);
-        StartPlane4.transform.position = new Vector3(-17, 0, 0);
-        GameObject StartPlane5 = Instantiate(StartTile, transform);
-        StartPlane5.transform.position = new Vector3(-25, 0, 0);
+        SpawnStartTiles();
     }
 
+    private void SpawnStartTiles()
+    {
+        float[] xPositions = { 7f, -1f, -9f, -17f, -25f };
+
+        for (int i = 0; i < xPositions.Length; i++)
+        {
+            GameObject startPlane = Instantiate(StartTile, transform);
+            startPlane.transform.position = new Vector3(xPositions[i], 0, 0);
+        }
+    }
+    
     private void Update()
     {
         gameObject.transform.position += new Vector3(_currentLevelSpeed * Time.deltaTime, 0, 0);
 
         if(transform.position.x >= Index)
         {
-            int RandomInt1 = Random.Range(0, 2);
-
-            if(RandomInt1 == 1)
-            {
-                SpawnTile(Tile1, new Vector3(-16, 0, 0));
-            }
-            else if(RandomInt1 == 0)
-            {
-                SpawnTile(Tile2, new Vector3(-16, 0, 0));
-            }
-
-            int RandomInt2 = Random.Range(0, 2);
-
-            if(RandomInt2 == 1)
-            {
-                SpawnTile(Tile1, new Vector3(-24, 0, 0));
-            }
-            else if(RandomInt2 == 0)
-            {
-                SpawnTile(Tile2, new Vector3(-24, 0, 0));
-            }
-
+            SpawnTile(_tiles[Random.Range(0, _tiles.Length)], new Vector3(-16, 0, 0));
+            SpawnTile(_tiles[Random.Range(0, _tiles.Length)], new Vector3(-24, 0, 0));
+            
             Index += 15.95f;
             _currentLevelSpeed += _speedFactor;
         }
