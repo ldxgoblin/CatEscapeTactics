d41d25297a622b2014822ec8fec3be33a9c91e10
Inácio Schweller <iscmendonca@gmail.com>
2023-07-29 19:49:37 +0200

    feat: done

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -4,15 +4,20 @@ using UnityEngine.SceneManagement;
 
 public class CollisionController : MonoBehaviour
 {
+    public AudioSource hit_source;
+    public AudioSource purr_source;
+
     public static event Action OnCatNipConsumed; 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Obstacle")
         {
-            SceneManager.LoadScene("Main");
+            hit_source.Play();
+            // SceneManager.LoadScene("Main");
         }
         else if (other.tag == "CatNip")
         {
+            purr_source.Play();
             OnCatNipConsumed?.Invoke();
             Debug.Log("CatNip Yoinked!");
         }
