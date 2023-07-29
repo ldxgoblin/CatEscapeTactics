1c5b96edb7a797f95d333b8bb690cb03b22178cc
SHC <superhardcode666@gmail.com>
2023-07-29 17:38:18 +0200

    scene update

--- Assets/Scripts/CatNipController.cs
+++ Assets/Scripts/CatNipController.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 public class CatNipController : MonoBehaviour
 {
     [SerializeField] private LevelGenerator _levelGenerator;
-    
+
     [SerializeField] private float _slowMotionFactor = .5f; // The amount to increase the speed by during the power-up.
     [SerializeField] private float _slowMotionDuration = 5.0f; // Duration of the power-up in seconds.
     [SerializeField] private float _lerpDuration = 1.0f; // Duration of the speed lerp when transitioning.
-    
-    private bool _isPowerUpActive = false;
+
+    private bool _isPowerUpActive;
 
     private void Awake()
     {
@@ -21,7 +20,7 @@ public class CatNipController : MonoBehaviour
     {
         CollisionController.OnCatNipConsumed -= ActivatePowerUp;
     }
-    
+
     // Call this method to activate the power-up.
     private void ActivatePowerUp()
     {
@@ -34,16 +33,17 @@ public class CatNipController : MonoBehaviour
 
     private IEnumerator DeactivatePowerUp()
     {
+        // for some reason, even though the levelgenerator is properly assigned, it can still be null
         if (_levelGenerator == null) yield break;
-        
-        float initialSpeed = _levelGenerator.GetLevelSpeed();
-        float targetSpeed = initialSpeed * _slowMotionFactor;
 
-        float elapsed = 0f;
+        var initialSpeed = _levelGenerator.GetLevelSpeed();
+        var targetSpeed = initialSpeed * _slowMotionFactor;
+
+        var elapsed = 0f;
 
         while (elapsed < _lerpDuration)
         {
-            float newSpeed = Mathf.Lerp(initialSpeed, targetSpeed, elapsed / _lerpDuration);
+            var newSpeed = Mathf.Lerp(initialSpeed, targetSpeed, elapsed / _lerpDuration);
             _levelGenerator.SetLevelSpeed(newSpeed);
             elapsed += Time.deltaTime;
             yield return null;
@@ -52,13 +52,15 @@ public class CatNipController : MonoBehaviour
         // Ensure the speed reaches the exact target value after the lerp.
         _levelGenerator.SetLevelSpeed(targetSpeed);
 
-        yield return new WaitForSeconds(_slowMotionDuration - (_lerpDuration * 2f)); // Subtract lerpDuration twice to account for both transitions.
+        yield return
+            new WaitForSeconds(_slowMotionDuration -
+                               _lerpDuration * 2f); // Subtract lerpDuration twice to account for both transitions.
 
         elapsed = 0f;
 
         while (elapsed < _lerpDuration)
         {
-            float newSpeed = Mathf.Lerp(targetSpeed, initialSpeed, elapsed / _lerpDuration);
+            var newSpeed = Mathf.Lerp(targetSpeed, initialSpeed, elapsed / _lerpDuration);
             _levelGenerator.SetLevelSpeed(newSpeed);
             elapsed += Time.deltaTime;
             yield return null;
